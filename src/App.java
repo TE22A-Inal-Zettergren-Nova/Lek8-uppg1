dea57c842f53361a28ec97bc0c65c5d44e088b4c
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-11-08 10:04:23 +0100

    bytte start värde till 2, klar med b)

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int varv =0;
+        int varv =2;
         while(varv<6)
         {
             System.out.println(varv);
