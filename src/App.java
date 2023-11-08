a2995351b98740945f15e90c44a73b9ec15d8529
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-11-08 10:03:15 +0100

    klar med a)-delen

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int varv =0;
+        while(varv<6)
+        {
+            System.out.println(varv);
+            varv++;
+        }
     }
 }
